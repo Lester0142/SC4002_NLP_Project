e62d55367ea8a1d6a2d2f70982eaa2639827028d
Dhanish <dhanish265@gmail.com>
2024-11-02 01:05:54 +0800

    fixing oov

--- part_1/oov.py
+++ part_1/oov.py
@@ -1,3 +1,4 @@
+import json
 import gensim
 import gensim.downloader
 import subword_nmt
@@ -5,10 +6,8 @@ import textblob
 from textblob import TextBlob
 from gensim.models import KeyedVectors
 
-
 glove_vectors = KeyedVectors.load("word2vec-google-news-300.model")
 words = set(glove_vectors.key_to_index.keys())
-# print("bythe" in words)
 
 def flatten(words):
     if isinstance(words, (list, tuple, set, range)):
@@ -37,25 +36,19 @@ def handle_oov(word):
         if res is not None:
             return res, secHalf
     return None
-            
-    
+          
+
 f = open("oov_words.txt", "r")
 oov_words = f.read().split("\n")
+dict = {}
+
 for i, word in enumerate(oov_words):
-    print("{}.".format(i), word, list(flatten(handle_oov(word))))
-
-# glove_vectors = gensim.downloader.load('word2vec-google-news-300')
-# # glove_vectors.save("word2vec-google-news-300.model")
-
-# word = 'ottosallies'
-# # word = input("input word : ")
-
-# try:
-#     vector = glove_vectors[word]
-#     print(vector)
-# except KeyError:
-#     #oov
-#     word = handle_oov(word)
-#     print(word)
-#     vector = glove_vectors[word]
-#     print(vector)
+    # print("{}.".format(i+1), word, end = " ")
+    res = list(flatten(handle_oov(word)))
+    # print(res)
+    dict[word] = res if res is not None else []
+f.close()
+        
+json_object = json.dumps(dict, indent = 4)
+with open("oovMap.json", "w") as f:
+    f.write(json_object)
